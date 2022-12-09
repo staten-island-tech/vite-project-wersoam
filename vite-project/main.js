f773f773f205f1852e94cbc44f5f8eadf60c0634
wersoam <cowistrash@gmail.com>
2022-12-09 10:11:38 -0500

    preemptive commit
    on click isn't working, back to event listener

--- vite-project/main.js
+++ vite-project/main.js
@@ -4,13 +4,17 @@ cards
   .filter((limited) => limited.type === "toy")
   .forEach((card) => console.log(card.name));
 //array check working
-const domselector = {
+const dom = {
   button: document.getElementById("input"),
-  box: document.getElementById("background"),
+  box: document.querySelector("#background"),
 };
-function theme() {
-  var box = document.body;
-  box.classList.toggle("light-mode");
+dom.button.addEventListener("click", function () {
+  theme();
+});
+function theme(background) {
+  var background = dom.box;
+  background.style.backgroundColor = "white";
 }
 //day 1 why does it not work ahhhhh(theme changing in progress)
 //day 2 trying to use on click now but its also not working
+// day 3 why is background and style null now?
